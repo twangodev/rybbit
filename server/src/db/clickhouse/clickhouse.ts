54c0dd43c797d70ec1152fe4718d4b1921086e68
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2025-07-01 21:45:00 -0700

    Add import_id to events table

--- server/src/db/clickhouse/clickhouse.ts
+++ server/src/db/clickhouse/clickhouse.ts
@@ -54,7 +54,8 @@ export const initializeClickhouse = async () => {
         ADD COLUMN IF NOT EXISTS cls Nullable(Float64),
         ADD COLUMN IF NOT EXISTS inp Nullable(Float64),
         ADD COLUMN IF NOT EXISTS fcp Nullable(Float64),
-        ADD COLUMN IF NOT EXISTS ttfb Nullable(Float64)
+        ADD COLUMN IF NOT EXISTS ttfb Nullable(Float64),
+        ADD COLUMN IF NOT EXISTS import_id Nullable(String)
     `,
   });
 
