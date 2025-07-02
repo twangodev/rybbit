dfe049b2d1d8dc9b12424db8ec9b092aa14e134e
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2025-07-01 21:53:31 -0700

    Create pg-boss connection

---
+++ server/src/lib/boss.ts
@@ -0,0 +1,16 @@
+import PgBoss from "pg-boss";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const boss = new PgBoss({
+    host: process.env.POSTGRES_HOST || "postgres",
+    port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+});
+
+boss.on("error", error => console.error(error));
+
+export default boss;
